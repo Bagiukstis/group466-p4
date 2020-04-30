8ae576984b150ab5c32b392dcee7341f48b53215
Valdas Druskinis <jedziukas14@gmail.com>
2020-04-30 11:42:55 +0200

    global header file

---
+++ jaco_control/src/global.h
@@ -0,0 +1,16 @@
+struct bottle {
+float x = -0.5;
+float y = -0.5;
+float z = 0.50;
+
+//Cup position
+/*float c_x = -0.7;
+float c_y = 0.0;
+float c_z = 0.45;
+*/
+};
+struct cup{
+  float x = -0.6;
+  float y = 0.3;
+  float z = 0.6;
+};
