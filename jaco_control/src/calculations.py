8d639c57f145ad190e3eec90c7043aecfe5b5352
Valdas Druskinis <jedziukas14@gmail.com>
2020-05-18 11:06:19 +0200

    noodle code v3

--- jaco_control/src/calculations.py
+++ jaco_control/src/calculations.py
@@ -4,6 +4,17 @@ import rospy
 from geometry_msgs.msg import PointStamped
 import numpy as np
 
+def transformation(x, y, z):
+	A = np.array([[0.222, 0.335, -0.9162, 0.3607], [0.9750, -0.0760, 0.2088, -0.5391], [0, -0.9397, -0.3420, 0.3899], [0, 0, 0, 1]],dtype = np.float64)
+	Pc = np.array([[x],[y],[z], [1]],dtype = np.float64)
+	Dotp= A.dot(Pc)
+	Final = np.round(Dotp, 3)
+
+	X = Final[0]
+	Y = Final[1]
+	Z = Final[2]
+	return X, Y, Z
+
 def callback(message):
 	rospy.loginfo(rospy.get_caller_id() + "Header %s", message.header.frame_id)
 	rospy.loginfo(rospy.get_caller_id() + "x = %.3f", message.point.x)
@@ -19,19 +30,9 @@ def callback(message):
 		Xc = message.point.x
 		Yc = message.point.y
 		Zc = message.point.z
-	def transformation(x, y, z):
-		A = np.array([[0.222, 0.335, -0.9162, 0.3607], [0.9750, -0.0760, 0.2088, -0.5391], [0, -0.9397, -0.3420, 0.3899], [0, 0, 0, 1]],dtype = np.float64)
-		Pc = np.array([[x],[y],[z], [1]],dtype = np.float64)
-		Dotp= A.dot(Pc)
-		Final = np.round(Dotp, 3)
-
-		X = Final[0]
-		Y = Final[1]
-		Z = Final[2]
-		return X, Y, Z
 	X_b, Y_b, Z_b = transformation(Xb, Yb, Zb)
 	X_c, Y_c, Z_c = transformation(Xc, Yc, Zc)
-	pb.PointStamped()
+	pb = PointStamped()
 	if message.header.frame_id == "Bottle":
 		pb.header.frame_id = "Bottle"
 		pb.point.x = X_b
@@ -42,14 +43,15 @@ def callback(message):
 		pb.point.x = X_c
 		pb.point.y = Y_c
 		pb.point.z = Z_c
+	rate = rospy.Rate(0.5)
 	while not rospy.is_shutdown():
 		pub.publish(pb)
 		rate.sleep()
 
 if __name__ == '__main__':
 	rospy.init_node('listenernpublisher')
-	rospy.Subscriber("cv/object_coordinates", PointStamped, callback)
 	pub = rospy.Publisher('chatter', PointStamped, queue_size = 10)
+	rospy.Subscriber("cv/object_coordinates", PointStamped, callback)
 	rospy.spin()
 
 """
